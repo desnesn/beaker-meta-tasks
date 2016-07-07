d602486faf3ceb6de95cda7d94e1d6da6d7f9bdb
Dan Callaghan <dcallagh@redhat.com>
2016-07-07 13:43:10 +1000

    task to test Skip result
    Change-Id: I7a7ca738267fe5fc81747ac903e9fe6e8ffee3bf

---
+++ Sanity/Skip-result/runtest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+. /usr/bin/rhts-environment.sh || exit 1
+
+report_result $TEST SKIP 0
